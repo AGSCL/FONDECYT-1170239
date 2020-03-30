0fc34c3a9a47ca1556576899736204ce32b17772
LACTM <gonzalez.santacruz.andres@gmail.com>
2020-03-29 22:17:50 -0300

    changes

--- a59ce30bf3a72a8a6df4a71290f8c646e47aa994/server.R
+++ a59ce30bf3a72a8a6df4a71290f8c646e47aa994/server.R
@@ -15,19 +15,23 @@ server <- function(input, output) {
       return(NULL)
       rio::import(inFile$datapath)
   })
+    getData_proc <- reactive({
+      rio::export(file= "data.sav", x=getData())
+      })
   output$contents <- renderTable(
     head(getData())
   )
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste("data-", Sys.Date(), ".sav", sep="")
+      paste("data", Sys.Date(), ".sav", sep="")
     },
     content = function(x) {
    #   rio::convert(input$infile$datapath, "mtcars.sav")
-export(file= "data.sav", x=data())
+      #https://www.rdocumentation.org/packages/rio/versions/0.5.16/topics/export
+      #https://stackoverflow.com/questions/57493392/how-to-fix-file-not-found-when-using-the-downloadhandler
+      rio::export(getData(),file=x)
     }
 )
   
 }
 
-shinyApp(ui = ui, server = server)
